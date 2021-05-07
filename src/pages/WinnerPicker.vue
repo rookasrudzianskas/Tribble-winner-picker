9c53d2e5ad030825dc43e388acc610c1f6c6d1ca
Rokas <rookas.rudzenskas@gmail.com>
2021-05-07 11:55:22 +0300

    Rokas

--- src/pages/WinnerPicker.vue
+++ src/pages/WinnerPicker.vue
@@ -7,7 +7,7 @@
 
     <q-card class="add-names column bg-red" v-if="!showWinnerScreen">
       <q-card-section>
-        <h2 class="text-center q-mt-none q-mb-md text-white text-bold">🔥 WINNER PICKER 🔥</h2>
+        <h2 class="text-center q-mt-none q-mb-md text-white text-bold deep-shadow">🔥 WINNER PICKER 🔥</h2>
 
         <q-input
           v-model="text"
@@ -31,7 +31,12 @@
     </q-card>
 
     <div  v-else class="winner-screen">
-      <p class="text-white">Winner screen</p>
+      <h1 class=" deep-shadow text-white absolute-top text-center text-bold">WINNER!! 🥰</h1>
+      <q-card dark class="bg-primary" bordered>
+        <q-card-section>
+          <div class="text-h6">Rookas</div>
+        </q-card-section>
+      </q-card>
     </div>
 
   </q-page>
@@ -53,6 +58,10 @@ export default {
 </script>
 
 <style lang="scss">
+.deep-shadow {
+  text-shadow: 0 -1px 0 #fff, 0 1px 0 #2e2e2e, 0 2px 0 #2c2c2c, 0 3px 0 #2a2a2a, 0 4px 0 #282828, 0 5px 0 #262626, 0 6px 0 #242424, 0 7px 0 #222, 0 8px 0 #202020, 0 9px 0 #1e1e1e, 0 10px 0 #1c1c1c, 0 11px 0 #1a1a1a, 0 12px 0 #181818, 0 13px 0 #161616, 0 14px 0 #141414, 0 15px 0 #121212, 0 22px 30px rgba(0,0,0,0.9);
+
+}
 .stage {
   background-image: url("../assets/background.jpg");
   background-size: cover;
@@ -75,9 +84,6 @@ export default {
 
 .add-names {
   z-index: 3;
-  h2 {
-    text-shadow: 0 -1px 0 #fff, 0 1px 0 #2e2e2e, 0 2px 0 #2c2c2c, 0 3px 0 #2a2a2a, 0 4px 0 #282828, 0 5px 0 #262626, 0 6px 0 #242424, 0 7px 0 #222, 0 8px 0 #202020, 0 9px 0 #1e1e1e, 0 10px 0 #1c1c1c, 0 11px 0 #1a1a1a, 0 12px 0 #181818, 0 13px 0 #161616, 0 14px 0 #141414, 0 15px 0 #121212, 0 22px 30px rgba(0,0,0,0.9);
-  }
   .q-textarea {
     textarea {
       font-size: 22px;
