58888ac782b241abb2475f24989bade3fe36a9ac
Rokas <rookas.rudzenskas@gmail.com>
2021-05-07 12:10:36 +0300

    Rokas

--- src/pages/WinnerPicker.vue
+++ src/pages/WinnerPicker.vue
@@ -53,15 +53,32 @@ export default {
     return {
       showWinnerScreen: false,
       namesText: '',
+      namesArray: [],
     }
   },
   methods: {
     pickWinner() {
       this.showWinnerScreen = true
+      this.initNamesArray()
+    },
+    initNamesArray() {
+      this.namesArray = this.namesText.split('\n')
+      console.log("🚀", this.namesArray)
     },
     startAgain() {
       this.showWinnerScreen = false
+    },
+    shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+      }
+      return a;
     }
+
   }
 }
 </script>
